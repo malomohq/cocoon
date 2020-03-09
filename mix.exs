cc1336775925cb135c060790a80aec3e42733cab
Kurt Friedrich <kurt@gomalomo.com>
2020-03-09 12:41:51 -0400

    ➕Add ex_doc as a dev dependency

--- mix.exs
+++ mix.exs
@@ -23,7 +23,8 @@ defmodule Cocoon.MixProject do
       # dev
       #
 
-      { :dialyxir, "~> 1.0-rc", only: :dev, runtime: false }
+      { :dialyxir, "~> 1.0-rc", only: :dev, runtime: false },
+      { :ex_doc, ">= 0.0.0", only: :dev, runtime: false }
     ]
   end
 end
