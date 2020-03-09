4cab2818dca99280fa6e0039a4dd67ab7de25a95
Kurt Friedrich <kurt@gomalomo.com>
2020-03-09 16:39:19 -0400

    ✨Add public function

--- lib/cocoon.ex
+++ lib/cocoon.ex
@@ -1,3 +1,25 @@
 defmodule Cocoon do
+  alias Cocoon.{ Transformer }
 
+  @doc """
+  Transforms data.
+
+  ## Arguments
+
+  * `:data` - map or list of maps to transform
+  * `:mappings` - list of tuple mappings
+
+  ## Examples
+
+    data = %{ "key" => "value" }
+    mappings = [{ :key, ["key"] }]
+
+    %{ key: "value" } = Cocoon.transform(data, mappings)
+
+    data = %{ "key" => 7 }
+    mappings = [{ :key, ["key"], &to_string/1 }]
+
+    %{ key: "7" } = Cocoon.transform(data, mappings)
+  """
+  defdelegate transform(data, mappings), to: Transformer, as: :call
 end
--- test/cocoon_test.exs
+++ test/cocoon_test.exs
@@ -1,3 +1,17 @@
 defmodule CocoonTest do
+  use ExUnit.Case, async: true
 
+  describe "transform/2" do
+    test "transforms data" do
+      v1 = Enum.random(["value1", "value2"])
+      v2 = Enum.random(1..10)
+      data = %{ "k1" => v1, "k2" => v2 }
+
+      mappings = [{ :k1, ["k1"] }, { :k2, ["k2"], &to_string/1 }]
+
+      v2_trans = to_string(v2)
+
+      assert %{ k1: ^v1, k2: ^v2_trans } = Cocoon.transform(data, mappings)
+    end
+  end
 end
